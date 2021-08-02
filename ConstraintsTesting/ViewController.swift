5c7906428bfc4dd7be00ee7b5309afb84cf095c9
Alexey Barinov <begoodbebad2012@gmail.com>
2021-08-02 19:43:02 +0300

    init

--- ConstraintsTesting/ViewController.swift
+++ ConstraintsTesting/ViewController.swift
@@ -8,10 +8,46 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var firstSquare: UIView = {
+       let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .red
+        return view
+    }()
+    
+    var secondSquare: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .green
+        return view
+    }()
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.addSubview(firstSquare)
+        view.addSubview(secondSquare)
+        view.backgroundColor = .blue
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        setupViews()
+    }
+    
+    private func setupViews() {
+        let constraints = [
+            firstSquare.topAnchor.constraint(equalTo: view.topAnchor, constant: 0),
+            firstSquare.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0),
+            firstSquare.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0),
+            secondSquare.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0),
+            secondSquare.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0),
+            secondSquare.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0),
+            secondSquare.heightAnchor.constraint(equalTo: firstSquare.heightAnchor, multiplier: 1),
+            secondSquare.topAnchor.constraint(equalTo: firstSquare.bottomAnchor, constant: 5)
+        ]
+        NSLayoutConstraint.activate(constraints)
     }
 
 
